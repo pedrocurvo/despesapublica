192d7ed53afcc495e5e4236d86d45a2529d79451
Clara Vasconcelos <clara.vasconcelos@junitec.pt>
2025-04-23 17:03:16 +0100

    efet_script_years

--- public/data/script_efet.py
+++ public/data/script_efet.py
@@ -1,3 +1,4 @@
+# 2023
 # Quadro 4.7. PO01 — Órgãos de Soberania: despesa por medidas do Programa
 # Quadro 4.9. PO02 — Governação: despesa por medidas do Programa
 # Quadro 4.16. PO03 — Representação Externa: despesa por medidas do Programa
@@ -20,9 +21,22 @@ import pandas as pd
 import json
 
 # Load the Excel file
-year = 2022
+year = 2023
 excel_path = f"Quadros_{year}.xlsx"
+
 sheet_name_list = ["Quadro 4.7.", "Quadro 4.9.", "Quadro 4.16.", "Quadro 4.18.", "Quadro 4.20.", "Quadro 4.22.", "Quadro 4.24.", "Quadro 4.26.", "Quadro 4.29.", "Quadro 4.32.", "Quadro 4.36.", "Quadro 4.46.", "Quadro 4.48.", "Quadro 4.54.", "Quadro 4.59.", "Quadro 4.62.", "Quadro 4.66."]
+
+sheet_name_list_2023 = ["Quadro 4.7.", "Quadro 4.9.", "Quadro 4.16.", "Quadro 4.18.", "Quadro 4.20.", "Quadro 4.22.", "Quadro 4.24.", "Quadro 4.26.", "Quadro 4.29.", "Quadro 4.32.", "Quadro 4.36.", "Quadro 4.46.", "Quadro 4.48.", "Quadro 4.54.", "Quadro 4.59.", "Quadro 4.62.", "Quadro 4.66."]
+sheet_name_list_2022 = ["Quadro 4.7.", "Quadro 4.9.", "Quadro 4.14.", "Quadro 4.16.", "Quadro 4.21.", "Quadro 4.23.", "Quadro 4.26.", "Quadro 4.28.", "Quadro 4.31.", "Quadro 4.34.", "Quadro 4.38.", "Quadro 4.48.", "Quadro 4.50.", "Quadro 4.56.", "Quadro 4.60.", "Quadro 4.63.", "Quadro 4.67."]
+sheet_name_list_2021 = ["Quadro 141", "Quadro 143", "Quadro 150", "Quadro 154", "Quadro 156", "Quadro 161", "Quadro 163", "Quadro 165", "Quadro 168", "Quadro 172", "Quadro 180", "Quadro 182", "Quadro 188", "Quadro 193", "Quadro 197", "Quadro 201", "Quadro 203"]
+sheet_name_list_2020 = ["Quadro 133", "Quadro 135", "Quadro 142", "Quadro 146", "Quadro 148", "Quadro 150", "Quadro 152", "Quadro 154", "Quadro 156", "Quadro 159", "Quadro 163", "Quadro 169", "Quadro 171", "Quadro 174", "Quadro 182", "Quadro 186", "Quadro 190", "Quadro 192"]
+sheet_name_list_2019 = ["Quadro 125", "Quadro 127", "Quadro 131", "Quadro 133", "Quadro 135", "Quadro 138", "Quadro 140", "Quadro 143", "Quadro 147", "Quadro 153", "Quadro 155", "Quadro 158", "Quadro 165", "Quadro 170", "Quadro 176", "Quadro 179", "Quadro 182"]
+sheet_name_list_2018 = ["Quadro 119", "Quadro 121", "Quadro 126", "Quadro 128", "Quadro 130", "Quadro 132", "Quadro 134", "Quadro 136", "Quadro 139", "Quadro 143", "Quadro 149", "Quadro 151", "Quadro 159", "Quadro 162", "Quadro 167", "Quadro 173", "Quadro 175", "Quadro 178"]
+sheet_name_list_2017 = ["Quadro 122", "Quadro 124", "Quadro 129", "Quadro 131", "Quadro 133", "Quadro 136", "Quadro 138", "Quadro 140", "Quadro 142", "Quadro 145", "Quadro 151", "Quadro 153", "Quadro 160", "Quadro 162", "Quadro 166", "Quadro 170", "Quadro 176", "Quadro 179"]
+sheet_name_list_2016 = ["Quadro 121", "Quadro 123", "Quadro 128", "Quadro 130", "Quadro 134", "Quadro 138", "Quadro 140", "Quadro 142", "Quadro 145", "Quadro 148", "Quadro 150", "Quadro 154", "Quadro 156", "Quadro 160", "Quadro 166", "Quadro 169", "Quadro 172"]
+sheet_name_list_2015 = ["Quadro 124", "Quadro 126", "Quadro 131", "Quadro 135", "Quadro 137", "Quadro 141", "Quadro 143", "Quadro 145", "Quadro 159", "Quadro 161", "Quadro 166", "Quadro 169", "Quadro 171", "Quadro 174"]
+sheet_name_list_2014 = ["Quadro 112", "Quadro 114", "Quadro 120", "Quadro 122", "Quadro 125", "Quadro 128", "Quadro 132", "Quadro 134", "Quadro 149", "Quadro 151", "Quadro 157", "Quadro 160", "Quadro 162", "Quadro 164"]
+
 output_path = f"quadros_{year}.json"
 
 # Read the sheets
